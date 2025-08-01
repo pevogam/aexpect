fe6973fcb5360c111b6570c2b3c4ac78f449d47e
Plamen Dimitrov <plamen.dimitrov@intra2net.com>
2025-08-01 20:13:41 +0800

    go beyond a helpful logging message on reused object server
    TODO: Provide proper reuse.

--- aexpect/remote_door.py
+++ aexpect/remote_door.py
@@ -626,6 +626,7 @@ def get_remote_object(
         remote_object = Pyro4.Proxy(f"PYRONAME:{object_name}@{host}:{port}")
         # noinspection PyProtectedMember
         remote_object._pyroBind()
+        # TODO: we should at least add new exposed and registered objects in this case
         LOG.warning("Previous remote object server running, reusing it as is")
     except Pyro4.errors.PyroError as error:
         if not session:
